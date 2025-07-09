02ecf768534451ed44318f2a848c1e939e5213c3
Felipe Ribeiro <fesribeiro0@gmail.com>
2025-07-09 18:14:49 -0300

    feat: header response

--- src/responses/responses.go
+++ src/responses/responses.go
@@ -8,6 +8,7 @@ import (
 
 // Response JSON default
 func JSON(w http.ResponseWriter, statusCode int, data any) {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
 	if err := json.NewEncoder(w).Encode(data); err != nil {
